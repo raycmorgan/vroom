4859b2d6ed514ff729eea0ee4383c17b3875caf0
RayMorgan <ray@simple-apps.com>
2009-10-07 21:31:21 -0700

    Now passing exception to the exception handler

--- test/app/exceptions.js
+++ test/app/exceptions.js
@@ -1,9 +1,21 @@
 exports.handlers = {
-  404: function () {
+  404: function (e) {
+    logError(this, e);
     return "[404] Oops.. the URL was not found.";
   },
   
-  500: function () {
+  500: function (e) {
+    logError(this, e);
     return "[500] Well, that didn't work. Sorry about that.";
+  }
+}
+
+function logError(request, e) {
+  if (e.stack) {
+    request.LOG.error(request.path + "\n" + e.stack + "\n\n");
+  } else if (e.message) {
+    request.LOG.error(request.path + " -- " + e.message + "\n");
+  } else {
+    request.LOG.error(request.path + "--" + JSON.stringify(e));
   }
 }
