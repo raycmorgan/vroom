5bea11470dddc305e2acb843ac75339aa7dbcfe0
RayMorgan <ray@simple-apps.com>
2009-10-08 19:44:41 -0700

    Made the test suite a prettier. Now outputs the successful and failed tests. As a side effect, multiple tests can fail and it will show them all.

--- test/test_suite.js
+++ test/test_suite.js
@@ -4,6 +4,10 @@ require("../lib/vroom.js");
 var rootPath = "./" + node.path.dirname(__filename).replace(node.cwd(), "") + "/mjsunit/";
 var contents = node.fs.readdir(rootPath).wait();
 var tests = [];
+var successful = 0;
+var errors = [];
+
+node.stdio.writeError("Running Vroom Test Suite\n\n");
 
 contents.forEach(function (filename) {
   file = rootPath + filename;
@@ -20,14 +24,45 @@ tests.forEach(function (t) {
       var scope = {};
       if (t.beforeEach)
         t.beforeEach.call(scope);
-      return testFun.call(scope);
+      try {
+        return testFun.call(scope);
+      } catch (e) {
+        return function () { throw(e); }
+      }
     });
     
     process.addListener('exit', function () {
       exitAsserts.forEach(function (t) {
-        if (typeof(t) == "function")
-          t();
+        try {
+          if (typeof(t) == "function") {
+            t();
+          }
+          node.stdio.writeError(".");
+          successful++;
+        } catch (e) {
+          node.stdio.writeError("f");
+          errors.push(e);
+        }
       });
     });
   }
 });
+
+process.addListener('exit', function () {
+  printErrors(errors);
+  node.stdio.writeError("\n\n" + successful + " Passed, " + errors.length + " Failures\n");
+});
+
+function printErrors(errors) {
+  for (var i = 0; i < errors.length; i++) {
+    var e = errors[i];
+    node.stdio.writeError("\n\n");
+    if (e.stack) {
+      node.stdio.writeError(e.stack);
+    } else if (e.message) {
+      node.stdio.writeError(e.message);
+    } else {
+      node.stdio.writeError(e);
+    }
+  }
+}
